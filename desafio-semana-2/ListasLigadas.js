094620c44bbbde9102d07967c345a56bcf997fcf
Torezann <gotorezan@gmail.com>
2025-02-22 13:33:06 -0300

    semana 2

--- desafio-semana-2/ListasLigadas.js
+++ desafio-semana-2/ListasLigadas.js
@@ -1,40 +1,62 @@
-class No {
-    constructor(valor) {
-      this.valor = valor;
-      this.proximo = null;
+class Main {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class ListaLigada {
+  constructor() {
+    this.head = null;
+  }
+
+  adicionar(value) {
+    const newMain = new Main(value);
+    if (!this.head) {
+      this.head = newMain;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newMain;
+  }
+
+  print() {
+    let current = this.head;
+    let result = '';
+    while (current) {
+      result += current.value + ' -> ';
+      current = current.next;
+    }
+    console.log(result + 'null');
+  }
+
+  remove(value) {
+    if (!this.head) return;
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next && current.next.value !== value) {
+      current = current.next;
+    }
+
+    if (current.next) {
+      current.next = current.next.next;
     }
   }
-  
-  class ListaLigada {
-    constructor() {
-      this.cabeca = null;
-    }
-  
-    adicionar(valor) {
-      const novoNo = new No(valor);
-      if (!this.cabeca) {
-        this.cabeca = novoNo;
-      } else {
-        let atual = this.cabeca;
-        while (atual.proximo) {
-          atual = atual.proximo;
-        }
-        atual.proximo = novoNo;
-      }
-    }
-  
-    imprimir() {
-      let atual = this.cabeca;
-      while (atual) {
-        console.log(atual.valor);
-        atual = atual.proximo;
-      }
-    }
-  }
-  
-  const lista = new ListaLigada();
-  lista.adicionar(1);
-  lista.adicionar(2);
-  lista.adicionar(3);
-  lista.imprimir();
-  
+}
+
+const list = new ListaLigada();
+list.adicionar(10);
+list.adicionar(20);
+list.adicionar(30);
+list.print();
+
+list.remove(20);
+list.print();
